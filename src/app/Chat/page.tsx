38ba077f6548b89f6940f9fe19216d6d3d0dceab
minahilf <sminahilfatima@gmail.com>
2025-07-01 09:26:33 +0500

    Day 4 completed

---
+++ src/app/Chat/page.tsx
@@ -0,0 +1,300 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Headphones } from "lucide-react";
+import ChatManager from "@/components/ChatManager";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../firebase";
+import { useRouter } from "next/navigation";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../../firebase";
+
+export default function ChatPage() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [showChatModal, setShowChatModal] = useState(false);
+  const [showChannelModal, setShowChannelModal] = useState(false);
+
+  const [message, setMessage] = useState("");
+  const [users, setUsers] = useState<any[]>([]);
+  const [selectedUserId, setSelectedUserId] = useState("");
+  const [chatMessages, setChatMessages] = useState<any[]>([]);
+  const [channels, setChannels] = useState<any[]>([]);
+  const [selectedChannelId, setSelectedChannelId] = useState("");
+
+  const router = useRouter();
+
+  const logOut = async () => {
+    await signOut(auth);
+    router.push("/Login");
+  };
+
+  useEffect(() => {
+    const currentUser = auth.currentUser;
+    if (!currentUser || !selectedUserId) return;
+
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const filtered = snapshot.docs
+        .map((doc) => doc.data())
+        .filter(
+          (msg) =>
+            (msg.senderId === currentUser.uid &&
+              msg.receiverId === selectedUserId) ||
+            (msg.senderId === selectedUserId &&
+              msg.receiverId === currentUser.uid)
+        );
+
+      setChatMessages(filtered);
+    });
+
+    return () => unsubscribe();
+  }, [selectedUserId]);
+
+  // fetching users from firestore
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "users"), (snapshot) => {
+      const userList = snapshot.docs.map((doc) => ({
+        uid: doc.id,
+        ...doc.data(),
+      }));
+      setUsers(userList);
+    });
+    return () => unsub();
+  }, []);
+
+  const sendMessage = async () => {
+    const currentUser = auth.currentUser;
+
+    if (
+      !currentUser ||
+      !message.trim() ||
+      (!selectedUserId && !selectedChannelId)
+    )
+      return;
+
+    try {
+      if (selectedUserId) {
+        await addDoc(
+          collection(db,"messages"),
+          {
+            senderId: currentUser.uid,
+            receiverId: selectedUserId,
+            text: message,
+            timestamp: serverTimestamp(),
+          }
+        );
+      } else if (selectedChannelId) {
+        await addDoc(
+          collection(db, "channels", selectedChannelId, "messages"),
+          {
+            senderId: currentUser.uid,
+            text: message,
+            timestamp: serverTimestamp(),
+          }
+        );
+      }
+
+      setMessage("");
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+
+  // channels msgs show krwane hen
+
+  useEffect(() => {
+    if (!selectedChannelId) return;
+
+    const q = collection(db, "channels", selectedChannelId, "messages");
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const msgs = snapshot.docs.map((doc) => doc.data());
+      setChatMessages(msgs);
+    });
+    return () => unsubscribe();
+  }, [selectedChannelId]);
+
+
+    const handleChannelClick = (channel: any) => {
+    const currentUser = auth.currentUser;
+    if (!currentUser) return;
+
+    if (channel.type === "private" && !channel.members.includes(currentUser.uid)) {
+      alert("You are not a member of this private channel");
+      return;
+    }
+
+    setSelectedChannelId(channel.id);
+    setSelectedUserId("");
+  };
+
+
+  return (
+    <div className="h-screen flex flex-col sm:flex-row font-poppins">
+      <aside
+        className={`
+          fixed top-0 left-0 h-full w-[350px] p-4 bg-mainPurple border-r z-50 
+          transform transition-transform duration-300 ease-in-out
+          ${showSidebar ? "translate-x-0" : "-translate-x-full"} 
+          md:relative md:translate-x-0 md:flex
+        `}
+      >
+        <div className="p-4 flex flex-col gap-2 w-full">
+          <div className="flex justify-between items-center">
+            <h2 className="text-xl font-bold text-white">Chattrix</h2>
+            <button
+              className="text-2xl pr-6 text-white sm:hidden"
+              onClick={() => setShowSidebar(false)}
+            >
+              ×
+            </button>
+          </div>
+
+          <button
+            className="w-full flex items-center gap-2 text-sm text-white font-bold mt-4"
+            onClick={() => setShowChatModal(true)}
+          >
+            <span className="bg-[#fec3ff] text-mainPurple rounded-md w-5 h-5 flex items-center justify-center font-bold">
+              +
+            </span>
+            New Chat
+          </button>
+
+          <button
+            className="w-full flex items-center gap-2 text-sm text-white font-bold"
+            onClick={() => setShowChannelModal(true)}
+          >
+            <span className="bg-[#fec3ff] text-mainPurple rounded-md w-5 h-5 flex items-center justify-center font-bold">
+              +
+            </span>
+            Join Channel
+          </button>
+
+          {/* mesaages user  */}
+
+          <h3 className="text-sm text-grayText font-bold mt-4">
+            Direct Messages
+          </h3>
+          <ul className="text-sm space-y-1 text-grayText cursor-pointer">
+            {users.map((user) => (
+              <li key={user.uid} onClick={() => {setSelectedUserId(user.uid);  setSelectedChannelId("");}}>
+                {user.name}
+              </li>
+            ))}
+          </ul>
+
+          {/* channels Name  */}
+
+          <h3 className="text-sm text-grayText font-bold mt-4">Channels</h3>
+          <ul className="text-sm space-y-1 text-grayText cursor-pointer">
+            {channels.map((channel) => (
+              <li
+                key={channel.id}
+                onClick={() => {handleChannelClick(channel); setSelectedUserId("");}}
+              >
+                #{channel.name}
+              </li>
+            ))}
+          </ul>
+
+          <button
+            onClick={logOut}
+            className=" mt-4 rounded-md w-[100px] h-[30px] font-medium text-[#4A154B] bg-[#fec3ff]"
+          >
+            Logout
+          </button>
+        </div>
+      </aside>
+
+      <main className="flex-1 bg-darkText flex flex-col">
+        <header className="flex items-center justify-between p-4 border-b border-gray-500 bg-darkText">
+          <button
+            className="md:hidden text-white text-xl"
+            onClick={() => setShowSidebar(!showSidebar)}
+          >
+            ☰
+          </button>
+          <h2 className="font-bold text-gray-300 text-xl"> {selectedUserId
+    ? users.find((u) => u.uid === selectedUserId)?.name || "User"
+    : selectedChannelId
+    ? "#" + (channels.find((c) => c.id === selectedChannelId)?.name || "Channel")
+    : "No Chat Selected"}</h2>
+
+           {selectedChannelId && (
+  <div className="flex flex-wrap gap-2 mt-2">
+    {[...new Set(channels.find((c) => c.id === selectedChannelId)?.members || []) as unknown as string[]].map((uid) => {
+      const user = users.find((u) => u.uid === uid);
+      if (!user) return null;
+      return (
+        <span
+          key={uid}
+          className="text-xs text-gray-300 font-medium"
+        >
+          {user.name}
+        </span>
+      );
+    })}
+  </div>
+)}
+
+          <Headphones className="text-white w-6 h-6 cursor-pointer" />
+
+   
+          
+        </header>
+
+
+
+        <ChatManager
+          showChatModal={showChatModal}
+          setShowChatModal={setShowChatModal}
+          showChannelModal={showChannelModal}
+          setShowChannelModal={setShowChannelModal}
+          setChannels={setChannels}
+          channels={channels}
+          users={users}
+          setSelectedUserId={setSelectedUserId}
+  setSelectedChannelId={setSelectedChannelId}
+        />
+
+        <div className="flex-1 overflow-y-auto p-4 space-y-3">
+          <div className="space-y-4">
+            {chatMessages.map((msg, index) => (
+              <div key={index} className="space-y-1">
+                <h4 className="font-bold text-white text-sm">
+                  {msg.senderId === auth.currentUser?.uid ? "You" : "Them"}
+                </h4>
+                <p className="text-white text-sm">{msg.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        
+
+        <footer className="p-4 border-t bg-[#333333] border-gray-500 flex gap-2">
+          <input
+            type="text"
+            placeholder="Type a message..."
+            onChange={(e) => setMessage(e.target.value)}
+            className="flex-1 p-2 rounded-md border bg-[#333333] border-[#333333] text-white focus:outline-none focus:border-gray-200"
+          />
+          <button
+            onClick={sendMessage}
+            className="bg-[#181818] text-white px-4 py-2 rounded-md"
+          >
+            Send
+          </button>
+        </footer>
+      </main>
+    </div>
+  );
+}
---
+++ src/components/ChatManager.tsx
@@ -0,0 +1,271 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  addDoc,
+  serverTimestamp,
+  collection,
+  doc,
+  getDoc,
+  updateDoc,
+  onSnapshot,
+} from "firebase/firestore";
+import { db, auth } from "../../firebase";
+
+// props ko ek interface dedia
+
+interface ChatManagerProps {
+  showChatModal: boolean;
+  setShowChatModal: (value: boolean) => void;
+  showChannelModal: boolean;
+  setShowChannelModal: (value: boolean) => void;
+  setChannels: (channels: any[]) => void;
+  channels: any;
+  users: any[];
+  setSelectedUserId: (id: string) => void;
+  setSelectedChannelId: (id: string) => void;
+}
+
+// just for ui dummy text agya
+
+// const dummyUsers = [
+//   { uid: "user1", name: "Minahil Fatima" },
+//   { uid: "user2", name: "Sara" },
+//   { uid: "user3", name: "Ahmed Raza" },
+// ];
+// const dummyChannels = ["#team", "#project", "#design"];
+
+export default function ChatManager({
+  // destructuring
+  showChatModal,
+  setShowChatModal,
+  showChannelModal,
+  setShowChannelModal,
+  setChannels,
+  channels,
+  users,
+   setSelectedUserId,     
+  setSelectedChannelId
+}: ChatManagerProps) {
+
+
+  // const [selectedUser, setSelectedUser] = useState("");
+  const [joinedChannels, setJoinedChannels] = useState<string[]>([]);
+  const [channelName, setChannelName] = useState("");
+  const [channelType, setChannelType] = useState("");
+  const [selectedMembers, setSelectedMembers] = useState<string[]>([]);
+  // const [channels, setChannels] =  useState<any[]>([])
+
+const handleJoin = async (channelId: string) => {
+  const currentUser = auth.currentUser;
+  if (!currentUser) return;
+
+  try {
+    const channelRef = doc(db, "channels", channelId);
+    const channelSnap = await getDoc(channelRef);
+
+    if (!channelSnap.exists()) return;
+
+    const data = channelSnap.data();
+    const currentMembers = data.members || [];
+
+    if (currentMembers.includes(currentUser.uid)) return;
+
+    
+    await updateDoc(channelRef, {
+      members: [...currentMembers, currentUser.uid],
+    });
+
+    setSelectedChannelId(channelId);
+    setSelectedUserId("");
+
+    alert("Joined channel successfully");
+
+  } catch (error) {
+    console.error("Error joining channel:", error);
+    alert("Join failed");
+  }
+};
+
+  // channel create krne ka form ki logic
+  const handleCreateChannel = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const currentUser = auth.currentUser;
+    if (!currentUser || !channelName || !channelType) return;
+
+    // database me store krhe hen
+    try {
+      await addDoc(collection(db, "channels"), {
+        name: channelName,
+        type: channelType,
+        admin: currentUser.uid,
+        members: [currentUser.uid, ...selectedMembers],
+        createdAt: serverTimestamp(),
+      });
+
+      setChannelName(""),
+        setChannelType(""),
+        setShowChannelModal(false),
+        alert("Channel created successfully");
+    } catch (error) {
+      alert("Something went wrong");
+    }
+  };
+
+  // usi channel ko jo db me store hua ab ui pe show krwayege
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "channels"), (snapshot) => {
+      const list = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setChannels(list);
+    });
+    return () => unsub();
+  }, []);
+
+  return (
+    <div className="h-0 w-0 overflow-hidden">
+      {showChatModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-50 flex items-center justify-center transition duration-300 ease-in-out">
+          <div className="bg-mainPurple rounded-lg p-6 w-full max-w-[500px] mx-auto shadow-lg animate-fade-in">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-2xl font-bold text-white">
+                Start a New Chat
+              </h2>
+              <button
+                onClick={() => setShowChatModal(false)}
+                className="text-2xl text-white"
+              >
+                ×
+              </button>
+            </div>
+
+            <select
+              onChange={(e) => {
+                const uid = e.target.value;
+                if (!uid) return;
+                setSelectedUserId(uid);
+                setShowChatModal(false); 
+                setSelectedChannelId("");
+              }}
+              className="w-full border p-2 rounded mb-4 focus:outline-none bg-[#943696] text-white"
+            >
+              <option value="">Select a user</option>
+              {users.map((user) => (
+                <option key={user.uid} value={user.uid} className="text-white">
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
+
+      {showChannelModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-50 flex items-center justify-center transition duration-300 ease-in-out">
+          <div className="bg-mainPurple rounded-lg p-6 w-full max-w-[500px] mx-auto shadow-lg animate-fade-in space-y-4">
+            <div className="flex justify-between items-center mb-2">
+              <h2 className="text-2xl font-bold text-white">Create Channels</h2>
+              <button
+                onClick={() => setShowChannelModal(false)}
+                className="text-2xl text-white"
+              >
+                ×
+              </button>
+            </div>
+
+            <form onSubmit={handleCreateChannel} className="space-y-4">
+              <input
+                type="text"
+                placeholder="Channel Name"
+                value={channelName}
+                onChange={(e) => setChannelName(e.target.value)}
+                className="w-full p-2 rounded bg-[#943696] text-white"
+              />
+
+              <select
+                value={channelType}
+                onChange={(e) => setChannelType(e.target.value)}
+                className="w-full p-2 rounded bg-[#943696] text-white"
+              >
+                <option value="">Select Channel Type</option>
+                <option value="public">Public</option>
+                <option value="private">Private</option>
+              </select>
+
+              {/* Show checkboxes only if private selected */}
+              {channelType === "private" && (
+                <div className="space-y-1">
+                  <h4 className="text-white font-bold">Select Members:</h4>
+                  {users.map((user) => (
+                    <label
+                      key={user.uid}
+                      className="flex items-center gap-2 text-white text-sm"
+                    >
+                      <input
+                        type="checkbox"
+                        value={user.uid}
+                        checked={selectedMembers.includes(user.uid)}
+                        onChange={() => {
+                          if (selectedMembers.includes(user.uid)) {
+                            setSelectedMembers(
+                              selectedMembers.filter((id) => id !== user.uid)
+                            );
+                          } else {
+                            setSelectedMembers([...selectedMembers, user.uid]);
+                          }
+                        }}
+                      />
+                      {user.name}
+                    </label>
+                  ))}
+                </div>
+              )}
+
+              <button
+                type="submit"
+                className="bg-green-500 text-white px-4 py-2 rounded w-full"
+              >
+                Create Channel
+              </button>
+            </form>
+
+            {/* channels show krna  */}
+
+            <div>
+              <h3 className="font-semibold mb-2 text-white">
+                Available Channels
+              </h3>
+              <ul className="space-y-2 text-[#a7a7a7]">
+                {channels.map((channel: any) => (
+                  <li
+                    key={channel.id}
+                    className="flex items-center justify-between text-sm"
+                  >
+                    <span>{channel.name}</span>
+
+                    {joinedChannels.includes(channel.id) ? (
+                      <span className="text-white bg-green-500 flex justify-center items-center font-medium w-[80px] rounded-md h-[30px]">
+                        Joined
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => handleJoin(channel.id)}
+                        className="text-white w-[80px] rounded-md h-[30px] bg-green-800 hover:underline"
+                      >
+                        Join
+                      </button>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
